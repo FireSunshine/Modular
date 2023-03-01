029e987c4b02b9218e4a63ed09be8d73e563c86d
sunshine <2898205320@qq.com>
2023-03-01 15:16:04 +0800

    配置 exclude 或 include 选项

---
+++ 04-webpack-demo/13-Increase-the-speed-of-packaging/src/heading.js
@@ -0,0 +1,16 @@
+import style from '../style/css/index.css';
+
+export default () => {
+  const element = document.createElement('h2');
+
+  element.textContent = 'hello world';
+
+  element.classList.add(style.box);
+
+  element.addEventListener('click', () => {
+    alert('hello world');
+    // hello();
+  });
+
+  return element;
+};
